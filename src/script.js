3e64ffaa5dae878fd5fbd409936b47247993d82c
Ricardo Cardoso <ricardomanolo90@gmail.com>
2025-07-12 21:59:03 -0300

    update commit

--- src/script.js
+++ src/script.js
@@ -26,10 +26,9 @@ function showQuestion() {
 
     let optionsHtml = "";
     for (let i in q.options) {
-      optionsHtml += `<div data-op="${i}" class="option"><span>${
-        Number(i) + 1
-      }</span>${q.options[i]}</div>`;
-    }
+      optionsHtml += `<div data-op="${i}" class="option"><span>${Number(i) + 1
+        }</span>${q.options[i]}</div>`;
+    };
     document.querySelector(".options").innerHTML = optionsHtml;
 
     document.querySelectorAll(".options .option").forEach((item) => {
@@ -37,18 +36,18 @@ function showQuestion() {
     });
   } else {
     finishQuiz();
-  }
-}
+  };
+};
 
 function optionClickEvent(e) {
   let clickedOption = Number(e.target.getAttribute("data-op"));
 
   if (questions[currentQuestion].answer === clickedOption) {
     correctAnswers++;
-  }
+  };
   currentQuestion++;
   showQuestion();
-}
+};
 
 function finishQuiz() {
   let points = Math.floor((correctAnswers / questions.length) * 100);
@@ -62,7 +61,7 @@ function finishQuiz() {
   } else if (points > 70) {
     document.querySelector(".scoreText1").innerHTML = "Parabéns!!";
     document.querySelector(".scorePct").style.color = "#0D630D";
-  }
+  };
 
   document.querySelector(".scorePct").innerHTML = `Acertou ${points}%`;
   document.querySelector(
@@ -72,10 +71,10 @@ function finishQuiz() {
   document.querySelector(".scoreArea").style.display = "block";
   document.querySelector(".questionArea").style.display = "none";
   document.querySelector(".progress--bar").style.width = "100%";
-}
+};
 
 function resetEvent() {
   correctAnswers = 0;
   currentQuestion = 0;
   showQuestion();
-}
+};
